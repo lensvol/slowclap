9ccec1f6d0ece47ee52c2dac6a83398ce4f7f6b5
unknown <C:\The Bat!\Mail>
2014-03-06 23:25:03 +0400

    Position within blocks are now saved.

--- slowclap/performances/management/commands/load_events_csv.py
+++ slowclap/performances/management/commands/load_events_csv.py
@@ -20,18 +20,29 @@ class Command(BaseCommand):
 
         count = 0
         for (category, pos, name, duration, block, day) in lines:
-            category_rec, _ = Category.objects.get_or_create(name=category)
+            if category:
+                category_rec, _ = Category.objects.get_or_create(name=category)
+            else:
+                category_rec = None
+
             start = datetime.datetime.strptime(start_date, '%d.%m.%Y')
             start += datetime.timedelta(days=int(day)-1)
             
-            block_rec, _ = ActionBlock.objects.get_or_create(name=u'Блок #{0}'.format(block),
-                                                             start=start)
+            if block:
+                block_rec, _ = ActionBlock.objects.get_or_create(name=u'Блок #{0}'.format(block),
+                                                                 start=start)
+            else:
+                block_rec = None
+
             duration_sec = float(duration.replace(',', '.')) * 60
 
-            event, _ = Event.objects.get_or_create(category=category_rec,
-                                                   block=block_rec,
-                                                   duration=duration_sec,
-                                                   description=name.replace(u'\n', u' / '))
+            event, cr = Event.objects.get_or_create(category=category_rec,
+                                                    block=block_rec,
+                                                    ord=pos or None,
+                                                    duration=duration_sec,
+                                                    description=name.replace(u'\n', u' / '))
             event.save()
+            if cr:
+                count += 1
 
         print '{0} events added from {1}.'.format(count, csv_fn)
--- slowclap/performances/models.py
+++ slowclap/performances/models.py
@@ -31,8 +31,14 @@ class Category(models.Model):
 
 
 class Event(models.Model):
-    block = models.ForeignKey(ActionBlock, verbose_name=u'Блок')
-    category = models.ForeignKey(Category, verbose_name=u'Категория')
+    block = models.ForeignKey(ActionBlock, 
+                              verbose_name=u'Блок', 
+                              null=True)
+    category = models.ForeignKey(Category, 
+                                 verbose_name=u'Категория', 
+                                 null=True, 
+                                 default=None)
+    ord = models.IntegerField(verbose_name=u'Номер в блоке', null=True)    
     description = models.CharField(max_length=255, 
                                    verbose_name=u'Описание',
                                    null=False,
