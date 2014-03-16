64b0d43537cb289b92b5911a2bbf5f9555ea00f9
Kirill Borisov <lensvol@gmail.com>
2014-03-16 18:07:49 +0400

    noscript-like placeholder (bug in Chrome).

--- slowclap/urls.py
+++ slowclap/urls.py
@@ -5,13 +5,10 @@ from django.conf.urls import patterns, include, url
 from slowclap.views import *
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'slowclap.views.home', name='home'),
-
-    url(r'roll$', event_roll),
-    url(r'noscript$', noscript_roll),
-    url(r'list/events$', EventList.as_view()),
-    url(r'list/blocks$', BlockList.as_view()),
-    url(r'list/categories$', CategoryList.as_view()),
-    url(r'list/program$', list_program)
+    url(r'/roll$', event_roll),
+    url(r'/noscript$', noscript_roll),
+    url(r'/list/events$', EventList.as_view()),
+    url(r'/list/blocks$', BlockList.as_view()),
+    url(r'/list/categories$', CategoryList.as_view()),
+    url(r'/list/program$', list_program)
 )
