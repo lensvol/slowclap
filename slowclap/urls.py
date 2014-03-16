2226e355c2cf689372e6d8bf765008de52614c94
Kirill Borisov <lensvol@gmail.com>
2014-03-16 22:42:27 +0400

    Fixed URLs.

--- slowclap/urls.py
+++ slowclap/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls import patterns, include, url
 from slowclap.views import *
 
 urlpatterns = patterns('',
-    url(r'/roll$', event_roll),
-    url(r'/noscript$', noscript_roll),
-    url(r'/list/events$', EventList.as_view()),
-    url(r'/list/blocks$', BlockList.as_view()),
-    url(r'/list/categories$', CategoryList.as_view()),
-    url(r'/list/program$', list_program)
+    url(r'^roll$', event_roll),
+    url(r'^noscript$', noscript_roll),
+    url(r'^list/events$', EventList.as_view()),
+    url(r'^list/blocks$', BlockList.as_view()),
+    url(r'^list/categories$', CategoryList.as_view()),
+    url(r'^list/program$', list_program)
 )
