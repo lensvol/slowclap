0607e899094e6d67c6d57b6d90de53de59579f7f
Kirill Borisov <lensvol@gmail.com>
2014-03-18 00:11:22 +0400

    Fixed displaying of timezone-aware dates.

--- slowclap/views.py
+++ slowclap/views.py
@@ -2,8 +2,10 @@
 
 import json
 import datetime
+import pytz
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from django.utils.timezone import get_current_timezone
 
 from rest_framework import generics
 from slowclap.models import Event, ActionBlock
@@ -18,18 +20,19 @@ def noscript_roll(request):
     blocks = ActionBlock.objects.filter(event__isnull=False)\
                                 .order_by('start')
     result = []
+    tz = get_current_timezone()
 
     for block in blocks:
         events = []
-
+        start_time = block.start.replace(tzinfo=pytz.utc).astimezone(tz)
         for ev in block.event_set.order_by('ord'):
-            start_time = block.start + datetime.timedelta(seconds=ev.duration)
             prepared_event = {
                 'start': start_time.strftime('%H:%M'),
                 'name': ev.description,
                 'category': ev.category.name
             }
             events.append(prepared_event)
+            start_time = start_time + datetime.timedelta(seconds=ev.duration)
 
         result.append({
             'name': block.name,
